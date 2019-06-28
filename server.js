52b313f063e0dedf31db27ab096c52529b1b3ac2
Joel Hernandez <joel24hernan@gmail.com>
2019-06-28 13:02:22 -0500

    Added .env.example file.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 require("dotenv").config();
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.db_password,
+  database: "greatbay_DB"
+});
