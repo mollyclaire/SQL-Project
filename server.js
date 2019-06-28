4ef5ca8c1ba983019d4abc8543065093bdce3333
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-06-28 13:28:29 -0500

    added start function

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
 require("dotenv").config();
+var inquirer = require("inquirer");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.db_password,
+  database: "greatbay_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "rawlist",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid.toUpperCase() === "POST") {
+        //CALL POST FUNCTION
+      }
+      else {
+       // CALL BID AUCTION
+      }
+    });
+}
+
