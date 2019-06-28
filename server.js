636809e0f78f3824c5b82daea4e577ab8fae29fa
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-28 13:09:35 -0500

    Initial files.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 require("dotenv").config();
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "process.env.DB_PASSWORD",
+    database: "playlist_db"
+  });
